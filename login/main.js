ec922d211a880f49eccadfb4e99cf1fb9a935836
Soohyeon Kim <tngusglaso@gmail.com>
2022-04-18 00:27:06 +0900

    레이아웃 수정

--- login/main.js
+++ login/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // 로그인 모달창 열기
 const loginBtn = document.querySelector('.login-box__btn');
 const loginModal = document.querySelector('#login-modal');
@@ -10,4 +12,12 @@ loginBtn.addEventListener('click', () => {
 const modalClose = document.querySelector('.header__close');
 modalClose.addEventListener('click', () => {
   loginModal.classList.add('invisible');
+});
+
+// 모달창
+const loginCheckBtn = document.querySelector('.login__check-btn');
+const loginCheckImg = document.querySelector('#login__check-img');
+
+loginCheckBtn.addEventListener('click', () => {
+  loginCheckImg.classList.toggle('invisible');
 });
